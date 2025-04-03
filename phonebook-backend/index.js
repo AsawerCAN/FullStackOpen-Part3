25f56878bbfd04c7d93ab16e80d1d1f2b639ae0c
Asawer Naseer <muhammadasawer75@gmail.com>
2025-04-03 22:27:44 +0500

    Exercise 3.3

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -22,6 +22,17 @@ app.get("/info", (req, res) => {
   `);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((p) => p.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
